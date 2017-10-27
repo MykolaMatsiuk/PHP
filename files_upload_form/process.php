977b16fa424ca6d78797d5a5a13926c961c4f842
Mykola Matsiuk <mykola.matsiuk@gmail.com>
2017-10-27 16:39:48 +0300

    1

--- files_upload_form/process.php
+++ files_upload_form/process.php
@@ -2,14 +2,14 @@
 require_once "functions.php";
 
 if (!empty($_POST) && !empty($_FILES['userfile'])) {
-if (!isset($_FILES['userfile'])) {
-  exit('File was not chosen!');
-};
-if ($_FILES['userfile']['size'] > 2000000) {
-  exit('File size must be less than 2MB');
-}
+  if (!isset($_FILES['userfile'])) {
+    exit('File was not chosen!');
+  };
+  if ($_FILES['userfile']['size'] > 2000000) {
+    exit('File size must be less than 2MB');
+  }
 
-saveFile();
+  saveFile();
 
-header("Location: index.php");
+  header("Location: index.php");
 }
