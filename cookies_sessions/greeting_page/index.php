157df936d60b6972430f7cfd45ca8f19a78c72d6
Mykola Matsiuk <mykola.matsiuk@gmail.com>
2017-11-02 21:07:39 +0200

    deleting

--- cookies_sessions/greeting_page/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-include_once "libs/Smarty.class.php";
-
-$smarty = new Smarty();
-$smarty -> setTemplateDir('template');
-$smarty -> display('index.tpl');
--- cookies_sessions/greeting_page/template/hello.tpl
+++
@@ -1,7 +0,0 @@
-{extends file='layout.tpl'}
-
-{block name=body}
-
-    <h1>Wellcome, {$smarty.session.user.name}</h1>
-
-{/block}
